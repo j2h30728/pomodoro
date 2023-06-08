5fd038719816fb24ff929cf33b1ed11cee3ba008
LeeJiHyeon <rachel2148072@gmail.com>
2023-06-08 12:36:09 +0900

    feat: 기본 형태 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ function App() {
   return (
     <>
       <h1>Pomodoro</h1>
+      <div>00 : 00</div>
+      <button>재생/일시정지</button>
+      <div>
+        <div>
+          <div>0/4</div>
+          <span>ROUND</span>
+        </div>
+        <div>
+          <div>0/12</div>
+          <span>GOAL</span>
+        </div>
+      </div>
     </>
   );
 }
